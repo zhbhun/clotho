1bff221852ac72ffe9d2a09e34860370a9873248
zhbhun <zhbhun@gmail.com>
2026-09-17 15:18:24 +0800

    feat: increase default tooltip show delay to 1000ms

--- src/shadcn/tooltip.tsx
+++ src/shadcn/tooltip.tsx
@@ -2,7 +2,7 @@ import { Tooltip as TooltipPrimitive } from '@base-ui/react/tooltip'
 
 import { cn } from '@/shadcn/utils'
 
-function TooltipProvider({ delay = 300, ...props }: TooltipPrimitive.Provider.Props) {
+function TooltipProvider({ delay = 1000, ...props }: TooltipPrimitive.Provider.Props) {
   return <TooltipPrimitive.Provider data-slot="tooltip-provider" delay={delay} {...props} />
 }
 
